de1496e683e6da3e820b550e25c72c7be9894030
ravi <rduttdogra@gmail.com>
2024-03-01 20:06:12 +0530

    init experssjs

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('raviduttdogra')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login in chai or backend</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>chai or backend</h2>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
